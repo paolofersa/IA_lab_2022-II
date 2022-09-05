212469b9beac3f18efc9c437bd817200334ff507
wairesp <54559794+wairesp@users.noreply.github.com>
2022-09-05 01:07:09 -0500

    added weights on a*

--- Search_algorithms/searchingPaths.py
+++ Search_algorithms/searchingPaths.py
@@ -1,4 +1,4 @@
-#REQUIRED LIBRARIES-----------------------------------------------------------------------
+# REQUIRED LIBRARIES-----------------------------------------------------------------------
 from ctypes import sizeof
 from os import remove
 import networkx as nx
@@ -9,18 +9,19 @@ import sys
 import random
 import os
 
-#AUXILIARY FUNCTIONS---------------------------------------------------------------------
-sys.setrecursionlimit(5000) #new max depth for interpreter stack
+# AUXILIARY FUNCTIONS---------------------------------------------------------------------
+sys.setrecursionlimit(5000)  # new max depth for interpreter stack
+
 
 def get_euclidean_distance(graph, node1, node2):
-    
     (x1, y1) = nx.get_node_attributes(graph, 'pos')[node1]
     (x2, y2) = nx.get_node_attributes(graph, 'pos')[node2]
-    return (((x2 - x1)**2 + (y2 - y1)**2)**0.5)
+    return (((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5)
 
-#MAIN FUNCTIONS--------------------------------------------------------------------------
+
+# MAIN FUNCTIONS--------------------------------------------------------------------------
 #   Graph Creation++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#EDUARDO
+# EDUARDO
 def initGrafo(G, num):
     aux = 0
     for i in range(0,num):
@@ -35,116 +36,117 @@ def initGrafo(G, num):
             actual = (num*i) + j
             if(j == 0):
                 G.add_node(actual, pos = (j, i))
-                G.add_edge(actual, (actual)-num, peso = 1)
-                G.add_edge(actual, (actual)-num + 1, peso = math.sqrt(2))
+                G.add_edge(actual, (actual)-num, peso = 2)
+                G.add_edge(actual, (actual)-num + 1, peso = math.sqrt(16))
             if (j == num-1):
                 G.add_node(actual, pos = (j, i))
-                G.add_edge(actual, (actual)-1, peso = 1)
-                G.add_edge(actual, (actual)-num - 1, peso = math.sqrt(2))
-                G.add_edge(actual, (actual)-num, peso = 1)
+                G.add_edge(actual, (actual)-1, peso = 2)
+                G.add_edge(actual, (actual)-num - 1, peso = math.sqrt(16))
+                G.add_edge(actual, (actual)-num, peso = 2)
             if(j > 0 and j < num-1):
                 G.add_node(actual, pos = (j, i))
-                G.add_edge(actual, (actual)-1, peso = 1)
-                G.add_edge(actual, (actual)-num - 1, peso = math.sqrt(2))
-                G.add_edge(actual, (actual)-num, peso = 1)
-                G.add_edge(actual, (actual)-num + 1, peso = math.sqrt(2))
+                G.add_edge(actual, (actual)-1, peso = 2)
+                G.add_edge(actual, (actual)-num - 1, peso = math.sqrt(16))
+                G.add_edge(actual, (actual)-num, peso = 2)
+                G.add_edge(actual, (actual)-num + 1, peso = math.sqrt(16))
+
 
 #   Brute force algorithms++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #   DFS
 def dfs(start, target, graph):
-        
     path = [[start]]
-    steps =0
+    steps = 0
     while path:
         steps = steps + 1
         index = -1
         s_path = path.pop(index)
-        l_node = s_path[-1] # the last node is our target, it's done
+        l_node = s_path[-1]  # the last node is our target, it's done
 
         if l_node == target:
-            print("Cantidad de Pasos -> {}" .format(steps))
+            print("Cantidad de Pasos -> {}".format(steps))
             return s_path
         else:
             for node in graph[l_node]:
                 if node not in s_path:
-                    new_path = s_path + [node]  
-                    path.append(new_path)               
-    print('El camino no existe' %(start, target))
+                    new_path = s_path + [node]
+                    path.append(new_path)
+    print('El camino no existe' % (start, target))
 
 
 #   BFS
 def bfs1(start, target, graph):
-    
     path = [[start]]
     steps = 0
     while path:
         steps = steps + 1
         index = 0
         s_path = path.pop(index)
-        l_node = s_path[-1] # the last node is our target, it's done
+        l_node = s_path[-1]  # the last node is our target, it's done
 
         if l_node == target:
-            print("Cantidad de Pasos -> {}" .format(steps))
+            print("Cantidad de Pasos -> {}".format(steps))
             return s_path
         else:
             for node in graph[l_node]:
                 if node not in s_path:
-                    new_path = s_path + [node]  
+                    new_path = s_path + [node]
                     path.append(new_path)
-    
-    print("El camino no existe {} {}" .format(start, target))                
-    
-    
+
+    print("El camino no existe {} {}".format(start, target))
+
+
 def delNodes(G, porcentaje, tam, ini, fin):
-    porcentajeInt = math.floor((tam*tam)*porcentaje/100)
-    for i in random.sample(range(0,tam*tam),porcentajeInt):
-        if(i != ini and i != fin):
+    porcentajeInt = math.floor((tam * tam) * porcentaje / 100)
+    for i in random.sample(range(0, tam * tam), porcentajeInt):
+        if (i != ini and i != fin):
             G.remove_node(i)
+
 
 #   Heuristics algorithms++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #   HILL CLIMBING
 
 
 #   A STAR
-def a_star_search(graph, initialNode, finalNode, steps = 0):
+def a_star_search(graph, initialNode, finalNode, steps=0):
     path = []
     path.append(initialNode)
-    #close condition: destination node reached
-    while(initialNode != finalNode):
-        #ask for the neighbors
+    # close condition: destination node reached
+    while (initialNode != finalNode):
+        # ask for the neighbors
         minWeight = 1000000
         minNeighbor = initialNode
         for neighbor in graph.neighbors(initialNode):
-            localWeight = graph[initialNode][neighbor]["peso"]    #calculate G(n)
-            localWeight += get_euclidean_distance(graph, neighbor, finalNode) #calculate H(n)
+            localWeight = graph[initialNode][neighbor]["peso"]  # calculate G(n)
+            localWeight += get_euclidean_distance(graph, neighbor, finalNode)  # calculate H(n)
             if (localWeight < minWeight):
                 minWeight = localWeight
                 minNeighbor = neighbor
-        steps+=1
+        steps += 1
         path.append(minNeighbor)
         initialNode = minNeighbor
 
-    print("Number of nodes visited was {}" .format(steps))
+    print("Number of nodes visited was {}".format(steps))
     return path
 
-#MAIN PROGRAM--------------------------------------------------------------------------
+
+# MAIN PROGRAM--------------------------------------------------------------------------
 G = nx.Graph()
 print("Elige el tamaño del grafo: ")
 tam = int(input())
-print("Escribe en que nodo inicia la busqueda del 0 al {}".format(tam*tam))
+print("Escribe en que nodo inicia la busqueda del 0 al {}".format(tam * tam))
 inicio = int(input())
-print("Escribe en que nodo termina la busqueda del 0 al {}".format(tam*tam))
+print("Escribe en que nodo termina la busqueda del 0 al {}".format(tam * tam))
 final = int(input())
 print("Escribe el porcentaje de nodos que quieres eliminar:")
 porcentaje = int(input())
 ## RECIBE EL GRAFO Y N PARA CREAR UN GRAFO DE N * N
 initGrafo(G, tam)
 
-delNodes(G,porcentaje,tam,inicio,final)
+delNodes(G, porcentaje, tam, inicio, final)
 
-res=[]
+res = []
 
-while(True):
+while (True):
 
     print("Que busqueda deseas realizar: \n")
     print("1. BFS \n")
@@ -152,59 +154,60 @@ while(True):
     print("3. HILL CLIMBING \n")
     print("4. A* \n")
     print("5. Salir")
-    
+
     option = int(input("-> "))
-    if(option == 1):
+    if (option == 1):
         res = bfs1(inicio, final, G)
         color_map = []
         for node in G:
             if node in res:
                 color_map.append('red')
-            else: 
+            else:
                 color_map.append('blue')
         print(res)
-        nx.draw(G, nx.get_node_attributes(G, 'pos'),node_color = color_map, with_labels=True)
+        nx.draw(G, nx.get_node_attributes(G, 'pos'), node_color=color_map, with_labels=True)
         plt.show()
 
-    if(option == 2):
+    if (option == 2):
         res = dfs(inicio, final, G)
         color_map = []
         for node in G:
             if node in res:
                 color_map.append('red')
-            else: 
+            else:
                 color_map.append('blue')
         print(res)
-        nx.draw(G, nx.get_node_attributes(G, 'pos'),node_color = color_map, with_labels=True)
+        nx.draw(G, nx.get_node_attributes(G, 'pos'), node_color=color_map, with_labels=True)
         plt.show()
 
-    #HILL CLIMBING
-    if(option == 3):
-        #HILL CLIMBING algorithm here
+    # HILL CLIMBING
+    if (option == 3):
+        # HILL CLIMBING algorithm here
         color_map = []
         for node in G:
             if node in res:
                 color_map.append('red')
-            else: 
+            else:
                 color_map.append('blue')
-        print("Cantidad de Pasos -> {}" .format(len(res)))
+        print("Cantidad de Pasos -> {}".format(len(res)))
         print(res)
-        nx.draw(G, nx.get_node_attributes(G, 'pos'),node_color = color_map, with_labels=True)
+        nx.draw(G, nx.get_node_attributes(G, 'pos'), node_color=color_map, with_labels=True)
         plt.show()
 
-    #A*
-    if(option == 4):
+    # A*
+    if (option == 4):
         res = a_star_search(G, inicio, final)
         color_map = []
         for node in G:
             if node in res:
                 color_map.append('red')
-            else: 
+            else:
                 color_map.append('blue')
         print(res)
         nx.draw(G, nx.get_node_attributes(G, 'pos'),node_color = color_map, with_labels=True)
+        nx.draw_networkx_edge_labels(G, nx.get_node_attributes(G, 'pos'), edge_labels=nx.get_edge_attributes(G, 'peso'))
         plt.show()
 
-    if(option == 5):
+    if (option == 5):
         break
     os.system('cls' if os.name == 'nt' else 'clear')
