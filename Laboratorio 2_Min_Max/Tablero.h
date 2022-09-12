8e88483bf1e8de55fe31631225e3b34f5abcab95
Eduardo <eduardo.segovia@ucsp.edu.pe>
2022-09-12 14:15:43 -0500

    Tree y Utilidad

--- Laboratorio 2_Min_Max/Tablero.h
+++ Laboratorio 2_Min_Max/Tablero.h
@@ -17,14 +17,6 @@
 #define PI 3.14159265358979323846  
 #define RADIUS 70
 
-# define colorRed glColor3f(1,0,0)
-# define colorYellow glColor3f(1,0.843,0)
-# define colorPurple glColor3f(0.502, 0.000, 0.502)
-# define colorGreen glColor3f(0,0.502,0)
-# define colorBlue glColor3f(0,0,0.804)
-# define colorBlack glColor3f(0,0,0)
-# define colorWhite glColor3f(1,1,1)
-
 #ifdef __APPLE__
 # include <OpenGL/gl.h>
 # include <OpenGL/glu.h>
@@ -36,41 +28,65 @@
 #endif
 #include <iostream>
 #include <utility>
+#include <iostream>
+#include <vector>
+#include "Tablero.h"
+
+# define colorRed glColor3f(1,0,0)
+# define colorYellow glColor3f(1,0.843,0)
+# define colorPurple glColor3f(0.502, 0.000, 0.502)
+# define colorGreen glColor3f(0,0.502,0)
+# define colorBlue glColor3f(0,0,0.804)
+# define colorBlack glColor3f(0,0,0)
+# define colorWhite glColor3f(1,1,1)
 
 using namespace std;
 struct Tablero{
-  int width;
-  int height; 
-  int casillas[3][3] = {{-1,-1,-1},{-1,-1,-1},{-1,-1,-1}};
-  pair<int,int> centros[3][3];
-
-  Tablero(int, int);
-
-  void dibujar();
-
-  bool marcarCasilla(int,int, bool&);
-
-  void dibujarTurnos();
-  
-  void drawEquis(pair<int,int> pos);
-  void drawHollowCircle (pair<int,int> pos);
-
-  private:
-    void marcarCentros();
+	int width;
+	int height; 
+	int jugadasRestantes;
+	int casillas[3][3] = {{-1,-1,-1},{-1,-1,-1},{-1,-1,-1}};
+	pair<int,int> centros[3][3];
+	
+	Tablero();
+	Tablero(int, int);
+	
+	void dibujar();
+	
+	bool marcarCasilla(int,int, bool&);
+	
+	void dibujarTurnos();
+	
+	void drawEquis(pair<int,int> pos);
+	void drawHollowCircle (pair<int,int> pos);
+	void copy(Tablero&);
+	
+private:
+	void marcarCentros();
 };
+
+void Tablero::copy(Tablero &newTablero) {
+	//cout<<newTablero.jugadasRestantes<<endl;
+	for ( int i = 0; i < 3; ++i ) {
+		for ( int j = 0; j < 3; ++j ) {
+			casillas[i][j] = newTablero.casillas[i][j];
+			jugadasRestantes = newTablero.jugadasRestantes;
+		}
+	}
+}
 
 void Tablero::drawEquis(pair<int,int> pos)
 {
 	colorGreen;
-  float divWidth = width / 3,
-  divHeight = height / 3;
-  int sizeW = divWidth /3;
-  int sizeH = divHeight/3;
+	float divWidth = width / 3,
+		divHeight = height / 3;
+	int sizeW = divWidth /3;
+	int sizeH = divHeight/3;
 	glBegin (GL_LINES);
-    glVertex2i(pos.first + sizeW, height - (pos.second + sizeH)); // der arriba
-    glVertex2i(pos.first - sizeW, height - (pos.second - sizeH));// izq abajo
-    glVertex2i(pos.first - sizeW, height - (pos.second + sizeH)); // izq arriba
-    glVertex2i(pos.first + sizeW, height - (pos.second - sizeH)); // der abajo
+	glVertex2i(pos.first + sizeW, height - (pos.second + sizeH)); // der arriba
+	glVertex2i(pos.first - sizeW, height - (pos.second - sizeH));// izq abajo
+	glVertex2i(pos.first - sizeW, height - (pos.second + sizeH)); // izq arriba
+	glVertex2i(pos.first + sizeW, height - (pos.second - sizeH)); // der abajo
 	glEnd ();
 }
 
@@ -82,47 +98,54 @@ void Tablero::drawHollowCircle (pair<int,int> pos)
 	int lineAmount = 100; //# of vertices used to draw circle
 	GLfloat twicePi = 2.0f * PI;
 	glBegin (GL_LINE_LOOP);
-		for ( i = 0; i <= lineAmount; i++ ) {
-			glVertex2f (
-						pos.first + (RADIUS * cos (i * twicePi / lineAmount)),
-						(height - pos.second ) + (RADIUS * sin (i * twicePi / lineAmount))
-						);
-		}
+	for ( i = 0; i <= lineAmount; i++ ) {
+		glVertex2f (
+					pos.first + (RADIUS * cos (i * twicePi / lineAmount)),
+					(height - pos.second ) + (RADIUS * sin (i * twicePi / lineAmount))
+					);
+	}
 	glEnd ();
 }
 
+Tablero::Tablero() {
+	this->width = 0;
+	this->height = 0;
+	jugadasRestantes = 9;
+	
+}
 Tablero::Tablero(int width, int height) {
-  this->width = width;
-  this->height = height;
-  marcarCentros();
+	this->width = width;
+	this->height = height;
+	jugadasRestantes = 9;
+	marcarCentros();
 }
 
 void Tablero::marcarCentros() {
-  float divWidth = width / 3,
-  divHeight = height / 3;
-  
-  centros[0][0] = make_pair(divWidth/2, divHeight/2);
-  centros[1][0] = make_pair(divWidth/2, (divHeight + divHeight*2)/2);
-  centros[2][0] = make_pair(divWidth/2, (divHeight*2 + divHeight*3)/2);
-  
-  centros[0][1] = make_pair((divWidth+divWidth*2)/2, divHeight/2);
-  centros[1][1] = make_pair((divWidth+divWidth*2)/2, (divHeight + divHeight*2)/2);
-  centros[2][1] = make_pair((divWidth+divWidth*2)/2, (divHeight*2 + divHeight*3)/2);
-  
-  centros[0][2] = make_pair((divWidth*2+divWidth*3)/2, divHeight/2);
-  centros[1][2] = make_pair((divWidth*2+divWidth*3)/2, (divHeight + divHeight*2)/2);
-  centros[2][2] = make_pair((divWidth*2+divWidth*3)/2, (divHeight*2+ divHeight*3)/2);
-  
-
-  // for (int i=0; i<3; i++)
+	float divWidth = width / 3,
+		divHeight = height / 3;
+	
+	centros[0][0] = make_pair(divWidth/2, divHeight/2);
+	centros[1][0] = make_pair(divWidth/2, (divHeight + divHeight*2)/2);
+	centros[2][0] = make_pair(divWidth/2, (divHeight*2 + divHeight*3)/2);
+	
+	centros[0][1] = make_pair((divWidth+divWidth*2)/2, divHeight/2);
+	centros[1][1] = make_pair((divWidth+divWidth*2)/2, (divHeight + divHeight*2)/2);
+	centros[2][1] = make_pair((divWidth+divWidth*2)/2, (divHeight*2 + divHeight*3)/2);
+	
+	centros[0][2] = make_pair((divWidth*2+divWidth*3)/2, divHeight/2);
+	centros[1][2] = make_pair((divWidth*2+divWidth*3)/2, (divHeight + divHeight*2)/2);
+	centros[2][2] = make_pair((divWidth*2+divWidth*3)/2, (divHeight*2+ divHeight*3)/2);
+	
+	
+	// for (int i=0; i<3; i++)
 	// {
 	// 	for(int j=0; j<3; j++)
 	// 	{
-  //       if(i == 0 && j ==0)
-  //       centros[i][j] = 123
-            
-  //   }
-  // }
+	//       if(i == 0 && j ==0)
+	//       centros[i][j] = 123
+	
+	//   }
+	// }
 }
 
 void Tablero::dibujarTurnos() {
@@ -130,7 +153,7 @@ void Tablero::dibujarTurnos() {
 	{
 		for(int j=0; j<3; j++)
 		{
-			if (!casillas[i][j])
+			if (casillas[i][j] == 0)
 			{
 				drawHollowCircle(centros[i][j]);
 			}
@@ -151,23 +174,23 @@ bool Tablero::marcarCasilla(int x, int y, bool  &turno) {
 	{
 		// pos: 0,0
 		if(y < divHeight && casillas[0][0] == -1){
-		casillas[0][0] = turno;
-		turno = !turno;
-		return 1;
+			casillas[0][0] = turno;
+			// turno = !turno;
+			return 1;
 		}
 		// pos: 1,0
 		else if(y > divHeight && y< divHeight*2 && casillas[1][0] == -1)
 		{
-		casillas[1][0] = turno;
-		turno = !turno;
-		return 1;
+			casillas[1][0] = turno;
+			// turno = !turno;
+			return 1;
 		}
 		// pos: 2,0
 		else if( y> divHeight*2 && y< divHeight*3 && casillas[2][0] == -1)
 		{
-		casillas[2][0] = turno;
-		turno = !turno;
-		return 1;	
+			casillas[2][0] = turno;
+			// turno = !turno;
+			return 1;	
 		}
 	}	
 	else if( x < divWidth*2 )
@@ -176,51 +199,53 @@ bool Tablero::marcarCasilla(int x, int y, bool  &turno) {
 		if(y < divHeight && casillas[0][1] == -1)
 		{
 			casillas[0][1] = turno;
-			turno = !turno;
+			// turno = !turno;
 			return 1;
 		}
 		// pos: 1,1
 		else if(y > divHeight && y< divHeight*2 && casillas[1][1] == -1)
 		{	
 			casillas[1][1] = turno; // 0 o 1
-			turno = !turno;
+			// turno = !turno;
 			return 1;				
 		}
 		// pos: 2,1
 		else if(y> divHeight*2 && y< divHeight*3 && casillas[2][1] == -1)
 		{
 			casillas[2][1] = turno;
-			turno = !turno;
+			// turno = !turno;
 			return 1;	
 		}
-
+		
 	}
 	else if ( x <= divWidth*3)
 	{
-    // pos: 0,2
+		cout<<"divHeight "<<divHeight<<endl;
+		cout<<"casillas[2][2]"<<casillas[2][2]<<endl;
+		// pos: 0,2
 		if(y < divHeight  && casillas[0][2] == -1 ){
 			casillas[0][2] = turno;
-			turno = !turno;
+			// turno = !turno;
 			return 1;
 		}
-    // pos: 1,2
+		// pos: 1,2
 		else if(y > divHeight && y< divHeight*2 && casillas[1][2] == -1)
 		{
 			casillas[1][2] = turno;
-			turno = !turno;
+			// turno = !turno;
 			return 1;				
 		}
 		
-    	// pos: 2,2
+		// pos: 2,2
 		else if(y> divHeight*2 && y< divHeight*3 && casillas[2][2] == -1)
 		{
 			casillas[2][2] = turno;
-			turno = !turno;
+			// turno = !turno;
 			return 1;	
 		}
 	}
 	return 0;
-
+	
 }
 
 void Tablero::dibujar()
@@ -244,4 +269,155 @@ void Tablero::dibujar()
 	glVertex2i(padding,divHeight*2); glVertex2i(width-padding,divHeight*2);
 	glEnd();
 }
+
+
+
+struct Node {
+	Tablero tablero;
+	int estado,  height;
+	int idxCadena;
+	int utilidad = 0;
+	Node *back; /// padre
+	vector<Node *> nodes;
+	
+	Node(int height, Node *back) {
+		this->height = height;
+		this->back = back;
+		estado = 0;
+		idxCadena = -1;
+	}
+	
+	/// 0=Maquina,1=Jugador
+	int calcularUtilidad() {
+		int jugadorV = 0, MaquinaV = 0;
+		// Verficar 3 en raya horizontal y vertical(cruz)
+		for ( int i = 0; i < 3; ++i ) {
+			bool horJugador, horMaquina;
+			bool verJugador, verMaquina;//gano en alguna orientacion
+			horJugador = true, horMaquina = true;
+			verJugador = true, verMaquina = true;
+			for ( int j = 0; j < 3; ++j ) {
+				if ( tablero.casillas[i][j] == 1 ) 
+					horMaquina = false; /// horizontal para X
+				if ( tablero.casillas[i][j] == 0 ) 
+					horJugador = false; /// horizontal para O
+				
+				if ( tablero.casillas[j][i] == 1 ) 
+					verMaquina= false; /// vertical para X
+				if ( tablero.casillas[j][i] == 0 ) 
+					verJugador = false; /// vertical para O
+			}
+			if ( horJugador ) ++jugadorV; if ( horMaquina ) ++MaquinaV;
+			if ( verJugador ) ++jugadorV; if ( verMaquina ) ++MaquinaV;
+		}
+		/// diagonal
+		bool digjugador = true, digMaquina = true;
+		bool digInversaJugador = true, digInversaMaquina = true;
+		for ( int i = 0; i < 3; ++i ) {
+			if ( tablero.casillas[i][i] == 1 ) digMaquina = false; /// diagonal(\) para X
+			if ( tablero.casillas[i][i] == 0 ) digjugador = false; /// diagonal(\) para O
+			
+			if ( tablero.casillas[i][2 - i] == 1 ) digInversaMaquina = false; /// diagonal(/) para X
+			if ( tablero.casillas[i][2 - i] == 0 ) digInversaJugador = false; /// diagonal(/) para O
+		}
+		if ( digjugador ) ++jugadorV;  if ( digMaquina ) ++MaquinaV;
+		if ( digInversaJugador ) ++jugadorV; if ( digInversaMaquina ) ++MaquinaV;
+		
+		utilidad = MaquinaV - jugadorV ;
+	}
+	
+	~Node () {
+		while ( !nodes.empty () ) {
+			Node *node = nodes.back ();
+			delete node;
+			nodes.pop_back ();
+		}
+	}
+};
+
+struct MinMax {
+	int maxHeight;
+	Node *root;
+	
+	void setProf(int maxHeight) {
+		this->maxHeight = maxHeight;
+		root = new Node(0, nullptr);
+		createTree(root, 1, maxHeight);
+	}
+	
+	MinMax(int maxHeight) {
+		this->maxHeight = maxHeight;
+		root = new Node(0, nullptr);
+		createTree(root, 1, maxHeight);
+	}
+	
+	void createTree(Node *actual, int height, int maxHeight) {
+		if(maxHeight == 0) return;
+		
+		for(int i = 0; i < 10 - height; ++i) {
+			Node *node  = new Node(height, actual);
+			actual->nodes.push_back(node);
+			createTree(node, height + 1, maxHeight - 1);
+		}
+	}
+	
+	void agregarJugada(Node* node, int turno) {
+		int pos = 0;
+		for(int i = 0; i< 3; ++i)
+			for(int j = 0; j< 3; ++j)
+			if(node->tablero.casillas[i][j] == -1) {
+			// copiamos el tablero al hijo
+			node->nodes[pos]->tablero.copy(node->tablero);
+			node->nodes[pos]->tablero.casillas[i][j] = turno;
+			node->nodes[pos]->tablero.jugadasRestantes--;
+			pos++;
+		}
+	}
+	//genero arbol con maxima altura como condicion de parada 
+	//
+	void generalArbol(Node *actual, int turno) {
+		if(actual->height == maxHeight || actual->tablero.jugadasRestantes == 0)  {
+			actual->calcularUtilidad(); // hoja o final
+			return;
+		}
+		agregarJugada(actual, turno);
+		for(int i=0; i< actual->tablero.jugadasRestantes;++i) {
+			generalArbol(actual->nodes[i], !turno);
+		}
+		
+		if(actual->height % 2 == 0) // altura par
+			maximo(actual);
+		else 
+			minimo(actual);
+	}
+	
+	void maximo(Node* actual) {
+		int idxElegido = 0;
+		for(int i=1; i<actual->tablero.jugadasRestantes;++i) {
+			if(actual->nodes[i]->utilidad > actual->nodes[idxElegido]->utilidad) 
+				idxElegido = i;
+		}
+		actual->idxCadena = idxElegido;
+	}
+	
+	void minimo(Node* actual) {
+		int idxElegido = 0;
+		for(int i=1; i<actual->tablero.jugadasRestantes;++i) {
+			if(actual->nodes[i]->utilidad < actual->nodes[idxElegido]->utilidad) 
+				idxElegido = i;
+		}
+		actual->idxCadena = idxElegido;
+	}
+	
+	void checkArbol(Tablero &tablero){
+		root->tablero.copy (tablero);
+		generalArbol(root, 0);//genera el arbol para el tablero actual
+		Node *jugadaMaquina = root->nodes[root->idxCadena];//caminoi a elegir
+		cout<<root->idxCadena<<endl;
+		
+		tablero.copy(jugadaMaquina->tablero); // copiamos el tablero elegido por la maquina
+	}
+};
+
 #endif
+
--- Laboratorio 2_Min_Max/main.cpp
+++ Laboratorio 2_Min_Max/main.cpp
@@ -1,91 +1,186 @@
 
 // #include <iostream>
 #include <math.h>
-#include "Tablero.h"
 
-# define KEY_ESC 27
-# define KEY_PLUS 43
-# define KEY_MINUS 45
-# define KEY_ENTER 13
-# define KEY_B 98
-# define KEY_R 114
+#include "Tablero.h"
+// #include "MinMax.h"
+
+#define KEY_ESC 27
+#define KEY_PLUS 43
+#define KEY_MINUS 45
+#define KEY_ENTER 13
+#define KEY_B 98
+#define KEY_R 114
 
 #define font18 GLUT_BITMAP_HELVETICA_18
 #define font12 GLUT_BITMAP_9_BY_15
 #define font24 GLUT_BITMAP_TIMES_ROMAN_24
 
-
-#define PI 3.14159265358979323846  
+#define PI 3.14159265358979323846
 #define radius 70
-
-# define colorRed glColor3f(1,0,0)
-# define colorYellow glColor3f(1,0.843,0)
-# define colorPurple glColor3f(0.502, 0.000, 0.502)
-# define colorGreen glColor3f(0,0.502,0)
-# define colorBlue glColor3f(0,0,0.804)
-# define colorBlack glColor3f(0,0,0)
-# define colorWhite glColor3f(1,1,1)
 
 #define width 800
 #define height 600
 
-Tablero tablero(width,height);
-
-
-void reshape_cb (int w, int h) {
-	if (w==0||h==0) return;
-	glViewport(0,0,w,h);
-	glMatrixMode (GL_PROJECTION);
-	glLoadIdentity ();
-	gluOrtho2D(0,w,0,h);
-	glMatrixMode (GL_MODELVIEW);
-	glLoadIdentity ();
+void reshape_cb(int w, int h)
+{
+	if (w == 0 || h == 0)
+		return;
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, w, 0, h);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
-
-
+int profundidad = 0;
+MinMax maquina(0);
+Tablero tablero(width, height);
 
 // int tablero[3][3] = {{-1,-1,-1},{-1,-1,-1},{-1,-1,-1}};
-bool turno = false; // 0 = maquina, 1 = jugador
+bool turno = true; // 0 = maquina, 1 = jugador
 
-
-void display_cb() {
+void display_cb()
+{
 	glClear(GL_COLOR_BUFFER_BIT);
-
+	
 	tablero.dibujar();
-
+	
 	tablero.dibujarTurnos();
-
+	
 	glutSwapBuffers();
 }
 
-void OnMouseClick(int button, int state, int x, int y) {
-//y = height - y
-	if ( (button == GLUT_LEFT_BUTTON) && state == GLUT_DOWN ) {
+int ganador = -1;
 
-		if ( tablero.marcarCasilla(x,y, turno)) {
-			
-		}
-		 std::cout <<x<<" "<<y<<std::endl;
+void checkGanador()
+{
+	if (!tablero.jugadasRestantes)
+	{
+		ganador = 2;
+		return;
 	}
-	glutPostRedisplay ();
+	bool horJugador, horMaquina;
+	bool verJugador, verMaquina;
+	bool digJugador = true, digMaquina = true;
+	bool _digJugador = true, _digMaquina = true;
+	for (int i = 0; i < 3; ++i)
+	{
+		horJugador = true, horMaquina = true;
+		verJugador = true, verMaquina = true;
+		for (int j = 0; j < 3; ++j)
+		{
+			if (tablero.casillas[i][j] != 0)
+				horMaquina = false; /// horizontal para X
+			if (tablero.casillas[i][j] != 1)
+				horJugador = false; /// horizontal para O
+			
+			if (tablero.casillas[j][i] != 0)
+				verMaquina = false; /// vertical para X
+			if (tablero.casillas[j][i] != 1)
+				verJugador = false; /// vertical para O
+		}
+		if (horJugador or verJugador)
+		{
+			ganador = 1;
+		}
+		if (horMaquina or verMaquina)
+		{
+			ganador = 0;
+		}
+	}
+	/// diagonal
+	for (int i = 0; i < 3; ++i)
+	{
+		if (tablero.casillas[i][i] != 0)
+			digMaquina = false; /// diagonal(\) para X
+		if (tablero.casillas[i][i] != 1)
+			digJugador = false; /// diagonal(\) para O
+		
+		if (tablero.casillas[i][2 - i] != 0)
+			_digMaquina = false; /// diagonal(/) para X
+		if (tablero.casillas[i][2 - i] != 1)
+			_digJugador = false; /// diagonal(/) para O
+	}
+	if (digJugador or _digJugador)
+	{
+		ganador = 1;
+	}
+	if (digMaquina or _digMaquina)
+	{
+		ganador = 0;
+	}
+	
+	if(ganador == 0)		
+		cout<<"El ganador es la maquina\n";
+	else if(ganador == 1)
+		cout<<"El ganador es el jugador\n";
 }
 
-void initialize() {
-	glutInitDisplayMode (GLUT_RGBA|GLUT_DOUBLE );// 255 or 0-1 float
-	glutInitWindowSize (width,height); // width, height de la ventana
-	glutInitWindowPosition (100,100);//incio ventana
-	glutCreateWindow ("Ventana OpenGL");//crea la ventana en base a las anteriores
-	glutDisplayFunc (display_cb); // Funcion que dibuja en la ventana
-	glutReshapeFunc (reshape_cb);//Resize de la ventana
-	glutMouseFunc (&OnMouseClick);
-	glClearColor(0.f,0.f,0.f,0.f);//blanco en la ventana al limpiar buffer
+void OnMouseClick(int button, int state, int x, int y)
+{
+	// y = height - y
+	if ((button == GLUT_LEFT_BUTTON) && state == GLUT_DOWN)
+	{
+		
+		if (tablero.marcarCasilla(x, y, turno))
+		{
+			tablero.jugadasRestantes--;
+			
+			if (tablero.jugadasRestantes)
+				maquina.checkArbol(tablero);
+			
+			checkGanador();
+		}
+		//std::cout << x << " " << y << std::endl;
+	}
+	glutPostRedisplay();
 }
 
-int main (int argc, char **argv) {
-	glutInit (&argc, argv);
-	initialize();//funcion importante
-	glutMainLoop();//ejecuta display
+void elegirTurno()
+{
+	string turn, dif;
+	cout << "Elega el turno\n"
+		<< "1. Jugador\n"
+		<< "2. Maquina\n";
+	cin >> turn;
+	cout << "Elega la dificultad\n"
+		<< "1. Facil\n"
+		<< "2. Medio\n"
+		<< "3. Dificil\n";
+	cin >> dif;
+	if (dif == "1")
+		profundidad = 3;
+	else if (dif == "2")
+		profundidad = 6;
+	else
+		profundidad = 9;
+	
+	maquina.setProf(profundidad);
+	if (turn == "2")
+		maquina.checkArbol(tablero);
+	turno = 1;
+}
+
+void initialize()
+{
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE); // 255 or 0-1 float
+	glutInitWindowSize(width, height);            // width, height de la ventana
+	glutInitWindowPosition(100, 100);             // incio ventana
+	glutCreateWindow("Ventana OpenGL");           // crea la ventana en base a las anteriores
+	glutDisplayFunc(display_cb);                  // Funcion que dibuja en la ventana
+	glutReshapeFunc(reshape_cb);                  // Resize de la ventana
+	glutMouseFunc(&OnMouseClick);
+	glClearColor(0.f, 0.f, 0.f, 0.f); // blanco en la ventana al limpiar buffer
+}
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	elegirTurno();
+	initialize();   // funcion importante
+	glutMainLoop(); // ejecuta display
 	return 0;
 }
 
